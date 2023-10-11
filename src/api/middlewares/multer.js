72c02812d804540c9da120d02ec7f65774db9db0
Dev Ashrit Behera <devashritcode@gmail.com>
2023-10-12 00:13:56 +0530

    image upload code

--- src/api/middlewares/multer.js
+++ src/api/middlewares/multer.js
@@ -1,16 +1,8 @@
 const multer =  require("multer");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname)
-    }
-})
-
 const upload = multer({
-    storage : storage,
+    dest : 'public/images/',
+    // storage : storage,
     limits : {
         fileSize : 1000000
     },
