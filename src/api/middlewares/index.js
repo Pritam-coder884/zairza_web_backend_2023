f73c7fc3ed7e32b696413dd8281197959b017599
Dev Ashrit Behera <devashritcode@gmail.com>
2023-10-11 23:51:28 +0530

    image upload code

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -1,10 +1,11 @@
 const auth=require("./auth");
 const notFound=require("./notFound")
 const errorHandlerMiddleware=require("./errorHandler")
-
+const upload = require('./multer')
 
 module.exports={
     auth,
     notFound,
-    errorHandlerMiddleware
+    errorHandlerMiddleware,
+    upload
 }
